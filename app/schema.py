03eb5cc5b57e11e6e3f7f11e36c4d3021ee9d109
MiguelPadillaR <m.padi.r@icloud.com>
2026-04-08 10:46:38 +0000

    chore: update `schema` and added ASTER MinIO credentials handling

--- app/schema.py
+++ app/schema.py
@@ -1,5 +1,5 @@
 schema = {
-    "name": "khaos-research/Satellite-Crop-Merge",
+    "name": "khaos-research/satellite-crop-merge",
     "description": """# Satellite Crop and Merge Downloader 
 An agrotech application to download satellite product data (images, indices etc.) using specific geometry. Products supported: EDAAn Data Space catalogue.
 
@@ -11,7 +11,7 @@ You can access the platform using the credentials provided by the platform admin
 
 The platform is organized into one single main section providing all specific inputs for data retrieval:
 
-- The Satellite Crop and Merge Downloader is an agrotech application designed for the EDAAn Data Space. It allows users to acquire different satellite product in the form of a compressed collection of TIF files. Data is cut from the geometry input the user provides. The application takes a determined date range, takes the images from the source (MinIO or Sentinel Hub) and performs the Crop & Merge operations
+- The Satellite Crop and Merge Downloader is an agrotech application designed for the EDAAn Data Space. It allows users to acquire different satellite product in the form of a compressed collection of TIF files. Data is cut from the geometry input the user provides. The application takes the specified date range, takes the images from the source (MinIO or Sentinel Hub) and performs the Crop & Merge operations.
 
 ### Input Information
 The application takes the following parameters for retrieval and download:
@@ -19,26 +19,24 @@ The application takes the following parameters for retrieval and download:
 - **Product key:** The selected product's ID.
   - The app's description on the interface provides a detail of which product keys correspond to what entries in the EDAAn product catalogue.
 - **Date range:** Date inputs to specify the temporal coverage.
-  - The dats must be in ISO format (YYYY-MM-DD).
+  - The dates must be in ISO format (YYYY-MM-DD).
   - MinIO takes only YYYY and MM to retrieve data since the database is made of monthly composites.
-  - Senintel Hub does need the exact date to calculate its coverage amd retrieve exact data.
+  - Sentinel Hub does need the exact date to calculate time coverage and retrieve exact data.
 - **Geometry input:** Specific parcel's geometry to crop the data from.
-  - Several options are offered:
-    - GeoJSON file upload: User will need to hace the geometry file prepared beforehand.
-    - SIGPAC Cadastral Reference: It will corss-reference the SIGPAC Spanish database to retrieve the geometry.
-    - Draw limits on map: The most intuitive form to input the geometry. Draw a polygon and process the data found on it.
+  - Several options are available:
+    - **GeoJSON file upload:** User will need to hace the geometry file prepared beforehand.
+    - **SIGPAC Cadastral Reference:** It will cross-reference the SIGPAC Spanish database to retrieve the geometry.
+    - **Draw limits on map:** The most intuitive form to input the geometry. Draw a polygon and process the data found on it.
 - **Data Source (TODO):** Choose between MinIO or Sentinel Hub as the source of the satellite products data
   - MinIO only covers some tiles, particularly, the ones in the Andalusia, Spain region.
-  - Sentinel Hub uses the public API to retrieve Sentinel-2 satellite data from virtually anywhere.
-...
+  - Sentinel Hub uses the public API to retrieve Sentinel-2 satellite data from virtually anywhere. Its use could potentially lead to hit API request limits.
 
 
 ### Output
 1. **Compressed Product Data files (ZIP)**:
    - The ZIP file contains an ordered logical directory structure whenre the TIF files can be found, along with some light documentation. 
-
 2. **Geometry GeoJSON file**:
-   - An optional file to download. It contains the geometry data used as a GeoJSON file. 
+   - Optional download file. It contains the geometry data used as a GeoJSON file. 
 
 ---
 
